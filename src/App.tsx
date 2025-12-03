56cb9c4c2ab4135ae97e41f23f58a885ef187f37
bishnu_kohar <koharbishnu322@gmail.com>
2025-12-03 20:22:34 +0545

    creating home page for daily routine

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container sx={{ marginTop: "px" }}>
+      <Typography variant="h4" component={"h1"} color="red">
+        Daily Todo Routine :
+      </Typography>
+
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+          marginTop: "10px",
+        }}
+      >
+        {/* input filed */}
+
+        <TextField
+          id="outlined-basic"
+          label="Task Name"
+          variant="outlined"
+          size="small"
+        />
+
+        {/* button */}
+        <Button variant="contained">Submit</Button>
+      </Box>
+    </Container>
+  );
 }
 
-export default App
+export default App;
